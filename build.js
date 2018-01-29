5611e3aea3f0c68503d4d612e7eecb69f5add7b1
Cheezmeister <github@luchenlabs.com>
2018-01-29 00:02:26 -0800

    Fix some stuff n document some stuff

--- build.js
+++ build.js
@@ -29,24 +29,25 @@ const Dir = {
 // Shorthands and Promisification
 const promisify = Node.util.promisify
 const identityFn = (a) => a
+const invert = (f) => (args) => !f(args)
+const wtf = (e) => console.log("WTF: " + red(e))
 const readFile = promisify(Node.fs.readFile)
 const writeFile = promisify(Node.fs.writeFile)
 const copyFile = promisify(Node.fs.copyFile)
 const mkdirp = promisify(Npm.mkdirp)
-const glob = (cwd, pattern) => new Promise((resolve, reject) => {
+const glob = (cwd, pattern) => new Promise(function(resolve, reject) {
   Npm.glob(pattern, {cwd: cwd}, function(err, files) {
     if (err) reject(err)
     else resolve(files)
   })
 })
-const renderStylus = (content, filename) => new Promise((resolve, reject) => {
-  Npm.stylus.render(content, {}, (err, css) => {
-    if (err) reject(err)
-    else resolve(css)
-  })
-})
 
-// Helpers
+// General Helpers
+function compose(...args) {
+  if (!args.length) return identityFn
+  const f = args.shift()
+  return (x) => compose(...args)(f(x))
+}
 const select = (field) => (object) => object[field]
 const color = (name) => (str) => {
   const termcode = (v) => `\x1b[${v}m`
@@ -86,51 +87,69 @@ async function bulkProcess(srcDir, files, destDir, munge, transformer) {
     return await writeFile(destfile, result)
   }))
 }
+
 function makeBulkTask(transformer) {
   return (srcDir, files, destDir, munge) =>
     bulkProcess(srcDir, files, destDir, munge, transformer)
 }
 
 async function index(template, srcDir, files, destDir) {
-  const metadata = (await glob(srcDir, files))
-    .filter((a) => -1 == a.indexOf('index.md')) // This is awful.
+  const isIndex = (a) => a.endsWith('index.md')
+  const filenames = await glob(srcDir, files)
+
+  let indexContent = {}
+  const indexFile = filenames.find(isIndex)
+  if (indexFile) {
+    const text = await readFile(`${srcDir}/${indexFile}`, 'utf8')
+    indexContent = prepContent(text)
+  }
+
+  const metadata = filenames
+    .filter(invert(isIndex))
     .map((f) => Node.fs.readFileSync(`${srcDir}/${f}`, {}).toString())
     .map(Npm.matter)
     .map(select('data'))
 
-  const html = Npm.pug.renderFile(template, {
-    index: metadata
-  })
+  const html = Npm.pug.renderFile(template, Object.assign(indexContent, {
+    index: metadata,
+  }))
   const destfile = `${destDir}/${Node.path.dirname(files)}/index.html`
-  await mkdirp(Node.path.dirname(destfile))
   console.log(`Index ${yellow(files)} (${srcDir}) => ${green(destfile)}`)
+  await mkdirp(Node.path.dirname(destfile))
   return await writeFile(destfile, html)
 }
 
-makeHighlighter = (extension) => {
+const makeHighlighter = (extension) => {
   const r = new Npm.marked.Renderer()
+  r.paragraph = (para, wat) => {
+    if (para.match(/^#!/)) {
+      return `<span class="shebang">${para}</span>`
+    }
+    return para
+  }
   r.code = (code, language) => {
     const lang = extension || language;
     const hl = Npm.hljs.highlightAuto(code, !!lang ? [lang] : undefined)
-    return Npm.pug.render(`pre: code.hljs.${'pl'} !{code}`, {code: hl.value})
+    return Npm.pug.render(`pre: code.hljs.${lang} !{code}`, {code: hl.value})
   }
   return r;
 }
 
-// e.g. 'README.coffee.md' => 'coffee', 'aoc.pl.md' => 'pl'
-highlightLiterate = (_, filename) => {renderer: makeHighlighter(getExtension(chopExtension(filename)))}
-const precompilePug = (template, extraOptions = (() => {})) => {
+const prepContent = (text, markedOptions = {}) => {
+  const page = Npm.matter(text)
+  const content = Npm.marked(page.content, Object.assign(markedOptions, {
+    breaks: false,
+    smartypants: true,
+  }))
+  return Object.assign({content: content}, page.data)
+}
 
+const precompilePug = (template, optionsCallback) => {
   const pugFunc = Npm.pug.compileFile(template)
   return makeBulkTask((text, filename) => {
-    const page = Npm.matter(text)
-    const extras = extraOptions(text, filename) || {};
-    const content = Npm.marked(page.content, Object.assign(extras, {
-      breaks: false,
-      smartypants: true,
-      renderer: makeHighlighter(filename.split('.').reverse()[1]),
-    }))
-    return pugFunc(Object.assign({content: content}, page.data))
+    const extras = optionsCallback ? optionsCallback(text, filename) : {};
+    const data = prepContent(text, extras)
+    return pugFunc(data)
   })
 }
 
@@ -139,36 +158,46 @@ const assets = async (srcDir, files, destDir) => {
   return Promise.all(filenames.map(async (f) => {
     const srcfile = Node.path.join(srcDir, f)
     const destfile = Node.path.join(destDir, f)
-    await copyFile(srcfile, destfile)
     console.log(`${cyan(f)} => ${green(destfile)}`)
     await mkdirp(Node.path.dirname(destfile))
     return await copyFile(srcfile, destfile)
-  }))
+  })).catch(wtf)
 }
 
-const stylus = makeBulkTask((text, filename) => renderStylus(text, filename))
+const renderPages = (template, optionsCallback) => precompilePug(`${Dir.layout}/${template}.pug`, optionsCallback)
 
-const renameMeRender = (template, o) => precompilePug(`${Dir.layout}/${template}.pug`, o)
+// e.g. 'README.coffee.md' => 'coffee', 'aoc.pl.md' => 'pl'
+const highlightLiterate = (_, filename) => {
+  return { renderer: makeHighlighter(getExtension(chopExtension(filename))) }
+}
 
-const tunes = renameMeRender('tunes')
-const games = renameMeRender('project')
-const home = renameMeRender('homepage')
-const pages = renameMeRender('page')
-const words = renameMeRender('page')
-const lprog = renameMeRender('page', highlightLiterate)
+const tunes = renderPages('tunes')
+const games = renderPages('project')
+const home = renderPages('homepage')
+const other = renderPages('page')
+const words = renderPages('page')
+const lprog = renderPages('page', highlightLiterate)
+const stylus = makeBulkTask((text, filename) => new Promise((resolve, reject) => {
+  Npm.stylus.render(text, {}, (err, css) => {
+    if (err) reject(err)
+    else resolve(css)
+  })
+}))
+
+
 
 
 try {
   home(Dir.content, 'index.md', Dir.deploy, extHTML)
-  lprog(Dir.content, 'lp/*.md', Dir.deploy, extHTML)
-  pages(Dir.content, '{bio,resume}.md', Dir.deploy, toPrettyURL)
+  lprog(Dir.content, 'lp/*.md', Dir.deploy, compose(chopExtension, toPrettyURL))
+  other(Dir.content, '{bio,resume}.md', Dir.deploy, toPrettyURL)
   tunes(Dir.content, 'tunes.md', Dir.deploy, toPrettyURL)
   games(Dir.content, 'projects/*.md', Dir.deploy, toPrettyURL)
   words(Dir.content, 'words/*.md', Dir.deploy, toPrettyURL)
   index(`${Dir.layout}/projectlist.pug`, Dir.content, 'projects/*.md', Dir.deploy)
   index(`${Dir.layout}/postlist.pug`, Dir.content, 'words/*.md', Dir.deploy)
   stylus(Dir.media, '**/*.styl', Dir.deploy, extCSS, { })
-  assets(Dir.media, '{images,icons}/**/*', `${Dir.deploy}/assets`)
+  assets(Dir.media, '{images,icons}/**/*.*', `${Dir.deploy}/assets`)
 } catch  (err) {
   console.log(`caught ${err}`)
 }
